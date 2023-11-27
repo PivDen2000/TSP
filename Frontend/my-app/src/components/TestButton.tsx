366462c73894014de3fa3e47f7a08bdb8b35bbff
Vladyslav Postrelko <vladyslav.postrelko@gmail.com>
2023-11-27 14:54:40 +0200

    changed node on frontend

--- Frontend/my-app/src/components/TestButton.tsx
+++ Frontend/my-app/src/components/TestButton.tsx
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const TestButton = () => {
-  const url = 'http://localhost:5201/Data';
+  const url = 'http://localhost:5201/SolveGraph';
 
   const getEmployees = async () => {
-    await axios.get<any>(url);
+    await axios.post<any>(url);
   }
   return (
     <div role="presentation" onClick={getEmployees}>
