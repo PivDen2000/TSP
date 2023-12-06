7ebf43de11217b49365ecd497c6d83f42e29389a
murphy <murphy@MURPHY-NB>
2023-12-06 18:25:48 +0200

    CoordinateDescentAlgorithm realization

--- Backend/Services/Algorithms/CoordinateDescentAlgorithm.cs
+++ Backend/Services/Algorithms/CoordinateDescentAlgorithm.cs
@@ -4,8 +4,71 @@ namespace Backend.Services.Algorithms;
 
 public class CoordinateDescentAlgorithm : IAlgorithm
 {
+    private List<string> _bestPath;
+    private int _bestCost;
+
     public List<string> Solve(Graph graph)
     {
-        throw new NotImplementedException();
+        _bestPath = new List<string>();
+        _bestCost = 0;
+
+        List<string> cities = graph.GetCities().ToList();
+        int numberOfCities = cities.Count;
+
+        int[][] costMatrix = new int[numberOfCities][numberOfCities];
+        foreach (var city1 in cities)
+        {
+            foreach (var city2 in cities)
+            {
+                costMatrix[graph.GetCityIndex(city1)][graph.GetCityIndex(city2)] = graph.GetDistance(city1, city2);
+            }
+        }
+
+        {   
+            int[] maxIndex;
+            int[] maxes = new int[numberOfCities];
+            int[] maxIndexes = new int[numberOfCities];
+            for (int i = 0; i < numberOfCities; i++)
+            {
+                maxes[i] = costMatrix[i].Max();
+                maxIndexes[i] = costMatrix[i].ToList().IndexOf(maxes[i]);
+            }
+            _bestCost = maxes.Max();
+            int maxind = maxes.ToList().IndexOf(_bestCost);
+            maxIndex = new int[] { maxind, maxIndexes[maxind] };
+            costMatrix[maxIndex[0], maxIndex[1]] = 0;
+            costMatrix[maxIndex[1], maxIndex[0]] = 0;
+            _bestPath.Add(cities[maxIndex[0]]);
+            _bestPath.Add(cities[maxIndex[1]]);
+        }
+        int n = 2;
+        while (n < numberOfCities)
+        {
+            int a = graph.GetCityIndex(_bestPath[0]);
+            int b = graph.GetCityIndex(_bestPath[-1]);
+            int maxCostA = costMatrix[a].Max();
+            int maxCostB = costMatrix[b].Max();
+            if (maxCostA >  maxCostB)
+            {
+                _bestCost += maxCostA;
+                _bestPath.Insert(0, cities[costMatrix[i].ToList().IndexOf(maxCostA);]);
+                for (int i = 0; i < numberOfCities; i++)
+                {
+                    costMatrix[i, a] = 0;
+                    costMatrix[a, i] = 0;
+                }
+            } else
+            {
+                _bestCost += maxCostB;
+                _bestPath.Add(cities[costMatrix[i].ToList().IndexOf(maxCostB);]);
+                for (int i = 0; i < numberOfCities; i++)
+                {
+                    costMatrix[i, b] = 0;
+                    costMatrix[b, i] = 0;
+                }
+            }
+            n++;
+        }
+        return _bestPath;
     }
 }
