4f5a673df19a0a16165297b4d1a050dd39c17fd0
Vladyslav Postrelko <vladyslav.postrelko@gmail.com>
2023-12-06 13:36:09 +0200

    added config file with constants, added result interface, added visualisation of result from backend

--- Frontend/my-app/src/components/TestButton.tsx
+++ Frontend/my-app/src/components/TestButton.tsx
@@ -1,15 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { data, url } from "../config/config";
+import styles from './TestButton.module.scss';
+
+interface ResultType {
+  inputRequest: {
+    cities: string[];
+    adjacencyMatrix: number[][];
+    algorithmType: string;
+  };
+  solution: {
+    optimalPath: string[];
+    totalDistance: number;
+  };
+}
 
 const TestButton = () => {
-  const url = 'http://localhost:5201/SolveGraph';
 
-  const getEmployees = async () => {
-    await axios.post<any>(url);
+  const [result, setResult] = useState<ResultType | null>(null);
+
+  const getResult = async () => {
+    try {
+      const response = await axios.post<ResultType>(url, data);
+      setResult(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setResult(null);
+    }
   }
   return (
-    <div role="presentation" onClick={getEmployees}>
-      Test Button
+    <div>
+      <div className={styles.container} role="presentation" onClick={getResult}>
+        Test Button
+      </div>
+      {result && (
+        <div>
+          <h2>Optimal Path:</h2>
+          <ul>
+            {result.solution.optimalPath.map(city => (
+              <li key={city}>{city}</li>
+            ))}
+          </ul>
+          <p>Total Distance: {result.solution.totalDistance}</p>
+        </div>
+      )}
     </div>
   );
 };
